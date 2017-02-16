75d44c8f417df0cf00d280551ce66e9be171df82
Drew Nutter <dnut@users.noreply.github.com>
2017-02-15 21:32:44 -0500

    fix crashes, lingering chars, improve meminfo grab
    This commit closes three issues:
    - Crashes when resizing: If the screen gets too small, catch the curses.error and refresh the screen. Lines 94 - 97
    - Lingering characters: Clear the entire screen every second. Lines: 91, 93
    - ZeroDivisionError: Catch error and set buffer to 0. Lines 63 - 67

--- buffer.py
+++ buffer.py
@@ -1,7 +1,6 @@
 import os
-import subprocess
+import itertools
 import time
-#import _thread
 import curses
 from copy import deepcopy as copy
 
@@ -12,7 +11,9 @@ def bold(msg):
 class System:
 	def __init__(self):
 		self.clean, self.count, self.average = 0, 0, 1
-		self.start = int(subprocess.check_output("grep -e Dirty: /proc/meminfo | awk '{ print $2 }'", shell=True)[:-1])
+		self.state = {}
+		self.meminfo()
+		self.start = self.state['Dirty']
 		self.max = copy(self.start)
 		self.total = copy(self.start)
 
@@ -20,17 +21,24 @@ class System:
 		os.system('sync')
 
 	def update(self):
-		self.dirty = int(subprocess.check_output("grep -e Dirty: /proc/meminfo | awk '{ print $2 }'", shell=True)[:-1])
-		self.wback = int(subprocess.check_output("grep -e Writeback: /proc/meminfo | awk '{ print $2 }'", shell=True)[:-1])
-		if self.dirty > self.max:
-			self.max = self.dirty
+		self.meminfo()
+		self.buffer = self.state['Dirty']
+		self.wrback = self.state['Writeback']
+		if self.buffer > self.max:
+			self.max = self.buffer
 		self.count += 1
-		self.average += (self.dirty - self.average) / self.count
-		new_clean = self.start - self.dirty
+		self.average += (self.buffer - self.average) / self.count
+		new_clean = self.start - self.buffer
 		if new_clean < self.clean:
 			self.total += self.clean - new_clean
-		self.clean = self.start - self.dirty
-		self.cleaned = self.total - self.dirty
+		self.clean = self.start - self.buffer
+		self.cleaned = self.total - self.buffer
+
+	def meminfo(self):
+		with open('/proc/meminfo') as meminfo:
+			for line in meminfo:
+				k, v = line.replace('kB', '').replace(' ', '').split(':')
+				self.state[k] = int(v)
 
 
 class UI:
@@ -52,20 +60,23 @@ class UI:
 
 	def report_progress(self):
 		width = self.screen.getmaxyx()[1] - 3
-		progress = (self.sys.average - self.sys.dirty) / self.sys.average
-		buffer = self.sys.dirty / self.sys.max
+		try:
+			#progress = (self.sys.average - self.sys.buffer) / self.sys.average
+			buffer = self.sys.buffer / self.sys.max
+		except ZeroDivisionError:
+			buffer = 0
 		#progress *= progress > 0
 		#progress2 = self.sys.cleaned / self.sys.total
 		#self.screen.addstr(0, 0, 'Initial buffer: {:{}} kB'.format(self.sys.start, 10))
-		self.screen.addstr(1, 0, 'Current buffer: {:{}} kB'.format(self.sys.dirty, 10))
+		self.screen.addstr(1, 0, 'Current buffer: {:{}} kB'.format(self.sys.buffer, 10))
 		self.screen.addstr(2, 0, 'Average buffer: {:{}} kB'.format(int(self.sys.average), 10))
 		self.screen.addstr(3, 0, 'Max buffer:     {:{}} kB'.format(self.sys.max, 10))
-		self.screen.addstr(5, 0, 'Writeback:      {:{}} kB'.format(self.sys.wback, 10))
+		self.screen.addstr(5, 0, 'Writeback:      {:{}} kB'.format(self.sys.wrback, 10))
 		self.screen.addstr(7, 0, 'Total buffered: {:{}} kB'.format(self.sys.total, 10))
 		self.screen.addstr(8, 0, 'Total flushed:  {:{}} kB'.format(self.sys.cleaned, 10))
 		#self.screen.addstr(11, 0, 'Progress: {:{}} % '.format(int(progress*100), 5))
 		#self.screen.addstr(12, 0, '[{:{}}]'.format('#'*int(progress*width),width))
-		if self.sys.dirty == self.sys.max:
+		if self.sys.buffer == self.sys.max:
 			self.screen.addstr(11, 0, 'Buffer enlarging, please wait.%s'.format(int(buffer*100), 5) % (' '*(width-30)))
 			self.screen.addstr(12, 0, '[%s]' % (' '*width))
 		else:
@@ -77,20 +88,17 @@ class UI:
 
 	def loop(self):
 		try:
-			#_thread.start_new_thread(self.input_thread, (L,))
-			#for i in range(100):
-			while 1:#self.sys.dirty > 0:
-				#if L: break
-				#ch = self.screen.getch()
-				#if ch >= 0:
-				#	break
+			for i in itertools.count():
 				self.sys.update()
-				self.report_progress()
+				self.screen.clearok(i % 10 == 0)
+				try:
+					self.report_progress()
+				except curses.error:
+					self.screen.refresh()
 				time.sleep(0.1)
 		except KeyboardInterrupt:
 			self.close()
-		finally:
-			self.close()
+		self.close()
 
 
 
